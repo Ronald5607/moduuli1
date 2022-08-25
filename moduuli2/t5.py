db4da8d6207c7939abd477befa6649beee25e1dd
roni <roni.paloniemi@metropolia.fi>
2022-08-25 10:39:14 +0300

    pieni muutos (mg->m)

--- moduuli2/t5.py
+++ moduuli2/t5.py
@@ -4,4 +4,4 @@ lu = float(input('Montako luotia?\n'))
 
 massa = (13.3*(lu + na*32 + le*32*20))
 
-print(f'Näiden massa on {massa/1000:.0f}kg ja {massa%1000:.0f}mg')
+print(f'Näiden massa on {massa/1000:.0f}kg ja {massa%1000:.0f}g')
