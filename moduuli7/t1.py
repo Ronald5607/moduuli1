eba906bed4eac4707fcfe7ea8c5b15a6bb331328
roni <roni.paloniemi@metropolia.fi>
2022-09-12 11:10:40 +0300

    a

--- moduuli7/t1.py
+++ moduuli7/t1.py
@@ -1,14 +1,9 @@
 import math
-#kuukaudet = ('Talvi','Talvi','Kevät',
-#             'Kevät','Kevät','Kesä',
-#             'Kesä','Kesä','Syksy',
-#             'Syksy','Syksy','Talvi')
-kuukaudet = ('Talvi', 'Kevät', 'Kesä', 'Syksy', 'Talvi')
 
+kuukaudet = ('Talvi', 'Kevät', 'Kesä', 'Syksy', 'Talvi')
 a = [math.ceil(x/3) for x in range(12)]
 
 kuukausi = int(input('Mikä kuukausi: '))
-
 if 0 < kuukausi <= 12:
     print(f'Kuukausi {kuukausi} kuuluu vuodenaikaan {kuukaudet[a[kuukausi-2]]}.')
 else:
