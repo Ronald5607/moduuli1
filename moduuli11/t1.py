df5e8e858e54025f6219878bffec3595d0ea1813
roni <roni.paloniemi@metropolia.fi>
2022-10-31 15:00:35 +0200

    moduuli11

--- moduuli11/t1.py
+++ moduuli11/t1.py
@@ -2,28 +2,28 @@ class Julkaisu:
     def __init__(self, nimi: str):
         self.nimi = nimi
 
-    # def tulosta_tiedot(self):
-    #     for key, value in self.__dict__.items():
-    #         print(f'{key}: {value}')
+    def tulosta_tiedot(self):
+        for key, value in self.__dict__.items():
+            print(f'{key}: {value}')
 
 
 class Kirja(Julkaisu):
     def __init__(self, nimi: str, kirjoittaja: str, sivumäärä: int):
-        self.nimi = nimi
+        super().__init__(nimi)
         self.kirjoittaja = kirjoittaja
         self.sivumäärä = sivumäärä
 
-    def tulosta_tiedot(self) -> None:
-        print(self.nimi, self.kirjoittaja, self.sivumäärä, sep=', ')
+    # def tulosta_tiedot(self) -> None:
+    #     print(self.nimi, self.kirjoittaja, self.sivumäärä, sep=', ')
 
 
 class Lehti(Julkaisu):
     def __init__(self, nimi: str, päätoimittaja: str):
-        self.nimi = nimi
+        super().__init__(nimi)
         self.päätoimittaja = päätoimittaja
 
-    def tulosta_tiedot(self) -> None:
-        print(self.nimi, self.päätoimittaja, sep=', ')
+    # def tulosta_tiedot(self) -> None:
+    #     print(self.nimi, self.päätoimittaja, sep=', ')
 
 
 """Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua)"""
